bd91addb4f1aaa5ba3b00d612f8d5153d0321277
kushk612 <kushk612@gmail.com>
2024-08-25 23:32:17 +0530

    add the props to index with child and functional components

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-function User() {
+function User(props) {
     const {userid} = useParams()
   return (
+    <>
     <div className='bg-orange-500 text-black text-3xl text-center py-5'>User: {userid}</div>
+    <h1>{props.rock}</h1>
+    </>
   )
 }
 
 export default User
+
+
